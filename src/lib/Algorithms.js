7dd9c13fd5374e5d8a827be290ee987c9515e417
Anand <anands.kartha@gmail.com>
2020-11-17 23:39:35 -0500

    Algorithm 11

--- src/lib/Algorithms.js
+++ src/lib/Algorithms.js
@@ -214,3 +214,29 @@ solution = (phrase) => {
     };
 }
 inputOutput.push(solution('hello world from Anand'));
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//////11. Ransom Note//////
+/////////////////////////////////////////////////////////////////////////////////////////////
+solution = (ransomeNote) => {
+    let magazine = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+    let output = true;
+    let ransomeNoteArr = ransomeNote.split(' '); //todo
+    console.log(ransomeNoteArr);
+    for(let i of ransomeNoteArr) {
+        if(magazine.includes(i)) {
+            magazine = magazine.split('').splice(i.indexOf(i), i.length).join('');
+            console.log(magazine);
+        } else {
+            output = false;
+            break;
+        }
+    }
+    return {
+        title: '11. Ransom Note',
+        input: ransomeNote,
+        output
+    };
+}
+inputOutput.push(solution('sit ad est sint'));
+inputOutput.push(solution('sit ad est love'));
