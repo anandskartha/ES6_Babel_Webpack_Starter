ada25b165c68b69b5ba676930a725b0297ddd8f8
Anand <anands.kartha@gmail.com>
2020-11-20 13:39:50 -0500

    ALgorithm 11 Issues fix

--- src/lib/Algorithms.js
+++ src/lib/Algorithms.js
@@ -220,13 +220,14 @@ inputOutput.push(solution('hello world from Anand'));
 /////////////////////////////////////////////////////////////////////////////////////////////
 solution = (ransomeNote) => {
     let magazine = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+    let magazineArr = [];
     let output = true;
     let ransomeNoteArr = ransomeNote.split(' '); //todo
-    console.log(ransomeNoteArr);
     for(let i of ransomeNoteArr) {
         if(magazine.includes(i)) {
-            magazine = magazine.split('').splice(i.indexOf(i), i.length).join('');
-            console.log(magazine);
+            magazineArr = magazine.split(' ');
+            magazineArr.splice(i.indexOf(i), i.length);
+            magazine = magazineArr.join(' ');
         } else {
             output = false;
             break;
